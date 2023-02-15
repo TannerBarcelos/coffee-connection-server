0413a3ceeb2d988bcf5a00a51fb340636b1bc383
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-02-15 14:08:27 -0800

    Cors updates and more

--- middleware/index.js
+++ middleware/index.js
@@ -2,11 +2,15 @@ const express = require( "express" )
 const initializeSession = require( "../lib/auth/session" )
 const setUpSessionMiddleware = require( "./sessionMiddleware" )
 const cors = require( "cors" )
+const { corsOpts, allowedOrigins } = require( '../lib/origins' )
+
+
 
 module.exports = function registerMiddlewares( app ) {
     app.use( express.json() )
     app.use( express.urlencoded( { extended: true } ) )
-    app.use( cors( { credentials: true, origin: true } ) )
+    app.use( cors( corsOpts ) )
     // app.use( initializeSession )
     // app.use( setUpSessionMiddleware )
 }
+
